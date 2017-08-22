ac64b008537ad8d6964e387414129ce28123da91
Tom Dobson <dobson980@gmail.com>
2017-08-21 22:16:31 -0700

    Initial Commit

---
+++ SimplyWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplyWeather
+//
+//  Created by Tom Dobson on 8/21/17.
+//  Copyright © 2017 Dobson Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
